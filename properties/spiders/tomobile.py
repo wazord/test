dbde391ef94e870bcaa39bf6806a50171325a13e
wzatxc <wangzhong2010@sina.com>
2020-04-16 00:42:04 +0800

    提交信息5

--- properties/spiders/tomobile.py
+++ properties/spiders/tomobile.py
@@ -27,28 +27,14 @@ class ToMobileSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        """ This function parses a property page.
-
-        @url http://scrapybook.s3.amazonaws.com/properties/property_000000.html
-        @returns items 1
-        @scrapes title price description address image_urls
-        @scrapes url project spider server date
-        """
-
+       
         # Create the loader using the response
         l = ItemLoader(item=PropertiesItem(), response=response)
 
         # Load fields using XPath expressions
-        l.add_xpath('title', '//*[@itemprop="name"][1]/text()',
-                    MapCompose(unicode.strip, unicode.title))
         l.add_xpath('price', './/*[@itemprop="price"][1]/text()',
                     MapCompose(lambda i: i.replace(',', ''), float),
                     re='[,.0-9]+')
-        l.add_xpath('description', '//*[@itemprop="description"][1]/text()',
-                    MapCompose(unicode.strip), Join())
-        l.add_xpath('address',
-                    '//*[@itemtype="http://schema.org/Place"][1]/text()',
-                    MapCompose(unicode.strip))
         l.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src',
                     MapCompose(lambda i: response.urljoin(response.url, i)))
 
